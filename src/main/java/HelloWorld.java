1c2c38bffcf01eb13fd1392d1c17c4f1fd558254
AlexanderMukas <alexandermukas@yahoo.com>
2018-06-18 23:36:23 +0300

    1 commit...

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.io.PrintStream;
+public class HelloWorld {
+
+    public static void print(PrintStream out){
+        out.println("Hello, World");
+    }
+    public static void main(String[] args) {
+        print(System.out);
+    }
+}
